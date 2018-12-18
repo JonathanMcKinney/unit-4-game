0a76617f8a05c5dd6852458a4f895dae5ed65cf4
J.McKinney <jsmckinney89@gmail.com>
2018-12-18 18:47:51 -0500

    finished javascript-added reset

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+
+
+
+var goal = Math.floor(Math.random() * 102 + 19); //create a random # between 19 and 120
+$("#ranGoal").text(goal);
+var guesses = [];
+var sum = 0;
+var loss = 0;
+var win = 0;
+
+function reset() {
+    $()
+    var goal = Math.floor(Math.random() * 102 + 19);
+    $("#ranGoal").text(goal);
+    var guesses = [];
+    sum = 0;
+    for (var i = 0; i < 4; i++) {
+        guesses.push(Math.floor(Math.random() * 12 + 1));
+        guess.attr("data-guessvalue", guesses[i]);
+        $(".guess").append(guess);
+    }
+}
+
+
+
+for (var i = 0; i < 4; i++) {
+    guesses.push(Math.floor(Math.random() * 12 + 1));
+    var guess = $("<img>");
+    guess.addClass("guess-image col-md-3");
+    guess.attr("src", "assets/images/ornament.jpg");
+    guess.attr("data-guessvalue", guesses[i]);
+    $(".guess").append(guess);
+}
+
+$(".guess-image").on("click", function () {
+    var guessValue = ($(this).attr("data-guessvalue"));
+    guessValue = parseInt(guessValue);
+    sum += guessValue;
+    $("#total").text(sum);
+
+    if (sum === goal) {
+        alert("You Win! Keep going!");
+        win++;
+        $("#winCount").text(win);
+        reset();
+
+    }
+    else if (sum >= goal) {
+        alert("Sorry, you lose.  Try again!");
+        loss++;
+        $("#lossCount").text(loss);
+        reset();
+    }
+})
